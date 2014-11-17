6e80694e6f6e668295ed58f73ab35c39c4016260
paulsoiya <psoiya@asu.edu>
2014-11-16 19:18:22 -0700

    PS11162014D updated code generation

--- src/org/igstk/validation/tools/codegeneration/CodeGeneration.java
+++ src/org/igstk/validation/tools/codegeneration/CodeGeneration.java
@@ -6,51 +6,80 @@ import java.util.HashMap;
 import org.igstk.validation.tools.codegeneration.XMLTransform;
 import org.igstk.validation.tools.codegeneration.Utility;
 
-
 public class CodeGeneration {
-	
+
 	private final String SCXML_DIR = "xmlFiles/scxmlFiles/";
-	
+
 	private final String OUTPUT_DIR = "output/";
-	
-	private final String TEMPLATE_FILE = "src/org/igstk/validation/tools/codegeneration/ClassTemplate.xsl";
-	
-	public CodeGeneration() { }
+
+	private final String TEMPLATE_FILE_PATH = "src/org/igstk/validation/tools/codegeneration/";
+
+	private Utility util = new Utility();
+
+	public CodeGeneration() {
+	}
 
 	public void generate(String scxmlFileName) {
-		
-		 File sourceFile, resultFile, templateFile;
-		 HashMap<String,String> params;
-		 Utility util = new Utility();
-		
-		 
-		 try {
-			 sourceFile = new File(SCXML_DIR + scxmlFileName); 
-			 String resultFileName = util.removeWord(sourceFile.getName(), ".xml");
-			 resultFile = new File(OUTPUT_DIR + resultFileName + ".cxx"); 
-			 templateFile = new File(TEMPLATE_FILE); 
-			 
-			 params	= new HashMap<String,String>();
-			 System.out.println(sourceFile.getName());
-			 
-			 String className = util.removeWord(sourceFile.getName(), "igstk");
-			 
-			 className = util.removeWord(className, ".xml");
-			 
-			 params.put("className", className);
-			 params.put("namespace", "igstk");
-			 
-			 XMLTransform t	= new XMLTransform();
-			 
-			t.transform(sourceFile, resultFile, templateFile, params);
-			 
-		 } catch(Exception e){
-			 System.out.println("exception thrown");
-		 }
 
+		String templates[] = {
+				TEMPLATE_FILE_PATH + "ContextHeaderTemplate.xsl",
+				TEMPLATE_FILE_PATH + "ContextClassTemplate.xsl",
+				TEMPLATE_FILE_PATH + "StateHeaderTemplate.xsl",
+				TEMPLATE_FILE_PATH + "StateClassTemplate.xsl" };
+
+		File sourceFile = new File(SCXML_DIR + scxmlFileName);
+
+		String resultFileName = OUTPUT_DIR + util.removeWord(sourceFile.getName(), ".xml");
 		
+		//generate the context header file
+		generateFile(sourceFile, resultFileName + ".h", TEMPLATE_FILE_PATH
+				+ "ContextHeaderTemplate.xsl");
+		
+		//generate the context class file
+		generateFile(sourceFile, resultFileName + ".cxx", TEMPLATE_FILE_PATH
+						+ "ContextClassTemplate.xsl");
+		
+		//generate the state header file
+		generateFile(sourceFile, resultFileName + "State.h", TEMPLATE_FILE_PATH
+				+ "ContextHeaderTemplate.xsl");
+		
+		//generate the state class file
+		generateFile(sourceFile, resultFileName + "State.cxx", TEMPLATE_FILE_PATH
+						+ "ContextClassTemplate.xsl");
+		
+		
+		
+
 	}
-	
+
+	private void generateFile(File sourceFile, String resultFileName,
+			String templateFileName) {
+
+		File resultFile, templateFile;
+		HashMap<String, String> params;
+
+		try {
+
+			resultFile = new File(resultFileName);
+			templateFile = new File(templateFileName);
+
+			params = new HashMap<String, String>();
+			System.out.println(sourceFile.getName());
+
+			String className = util.removeWord(sourceFile.getName(), "igstk");
+
+			className = util.removeWord(className, ".xml");
+
+			params.put("className", className);
+			params.put("namespace", "igstk");
+
+			XMLTransform t = new XMLTransform();
+
+			t.transform(sourceFile, resultFile, templateFile, params);
+
+		} catch (Exception e) {
+			System.out.println("exception thrown");
+		}
+	}
 
 }
-
